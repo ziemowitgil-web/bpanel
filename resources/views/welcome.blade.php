347bd06cf2198d6ec9d5b2e334701ad7540d5988
ziemowit-gil <ziemowit.gil@gmail.com>
2025-11-08 23:01:25 +0100

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,10 +8,17 @@
             <p class="fs-5">
                 Witaj! Obecnie system dla beneficjentów służy tylko do generowania przekierowań na zajęcia.
             </p>
-            <p class="text-muted">
+            <p class="text-muted mb-4">
                 Panel zostanie wkrótce rozbudowany o nowe funkcje.
             </p>
-            <i class="bi bi-arrow-repeat display-2 text-secondary mt-4"></i>
+            <i class="bi bi-arrow-repeat display-2 text-secondary mb-4"></i>
+
+            <div class="alert alert-info mt-4">
+                <i class="bi bi-exclamation-circle me-2"></i>
+                Uwaga! <strong>12.11</strong> nastąpi zmiana domeny z
+                <strong>feer-kr.edu.pl</strong> na <strong>edukacja.cloud</strong>.
+                Wraz z tym zmianie ulegną również <strong>loginy do Microsoft 365 dla edukacji</strong> 😊
+            </div>
         </div>
     </div>
 @endsection
